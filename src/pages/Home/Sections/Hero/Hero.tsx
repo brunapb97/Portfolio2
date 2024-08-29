10c5c13f34cf698d901921afb6cde82fb40ffbc6
brunapb97 <159681053+brunapb97@users.noreply.github.com>
2024-08-28 22:03:05 -0300

    alteracoes-contem-erro

---
+++ src/pages/Home/Sections/Hero/Hero.tsx
@@ -0,0 +1,28 @@
+import { styled } from "@mui/material";
+import Avatar from "../../../../assets/images/Avatar.jpeg"
+
+
+const Hero = () => {
+
+    const StyledHero = styled ("div")(()=>({
+        backgroundColor: "black",
+    }));
+
+    const StyledImg = styled ("img")(()=>({
+        width: "30%;",
+        borderRadius:"50%",
+         
+
+    }));
+
+    return (
+      <>
+        <StyledHero> 
+            Olá
+            <StyledImg src={Avatar}/>
+        </StyledHero>
+      </>
+    );
+  };
+  
+  export default Hero
